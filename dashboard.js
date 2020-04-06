8fbdea536b36585a84b437fbb686f7b946f3db3e
quatmax <quatmax@gmail.com>
2020-04-06 22:39:19 +0200

    new model ok

--- dashboard.js
+++ dashboard.js
@@ -1,37 +1,40 @@
-class File {
-    constructor(file, onFinished) {
-        this.filename = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_" + file + "_global.csv";
-        this.onFinished = onFinished;
-    }
-    visitFile(visitor) {
-        var dataFile = new XMLHttpRequest();
-        dataFile.open("GET", filename, true);
-        dataFile.onreadystatechange = function () {
-            if (dataFile.readyState === 4) {
-                if (dataFile.status === 200 || dataFile.status == 0) {
-                    visitor(dataFile.responseText.split(/\r\n|\n/));
-                    if(this.onFinished != undefined) {
-                        this.onFinished();
-                    }
-                }
+
+function getFileName(file) {
+    return "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_" + file + "_global.csv";
+}
+
+function visitFile(file, visitor) {
+    var filename = getFileName(file);
+    var dataFile = new XMLHttpRequest();
+    dataFile.open("GET", filename, true);
+    dataFile.onreadystatechange = function () {
+        if (dataFile.readyState === 4) {
+            if (dataFile.status === 200 || dataFile.status == 0) {
+                visitor(dataFile.responseText.split(/\r\n|\n/));
             }
-        };
-        dataFile.send();
-    }
-    visitLineData(visitLine) {
-        visitFile(function(allTextLines){
-            for (index = 1; index < allTextLines.length; index++) {
-                var line = allTextLines[index];
-                visitLine(line.split(/,/));
-            }
-        });
-    }
-    visitLabels(visitLabels) {
-        visitFile(function (allTextLines) {
-            visitLabels(allTextLines[0].split(/,/).slice(4));
-        });
-    }
-};
+        }
+    };
+    dataFile.send();
+}
+function visitLineData(file, visitLine, onFinished) {
+    visitFile(file, function(allTextLines){
+        for (var index = 1; index < allTextLines.length; index++) {
+            var line = allTextLines[index];
+            visitLine(line.split(/,/));
+        }
+        if(onFinished != undefined) {
+            onFinished();
+        }
+    });
+}
+function visitLabels(file, visitLabels, onFinished) {
+    visitFile(file, function (allTextLines) {
+        visitLabels(allTextLines[0].split(/,/).slice(4));
+        if(onFinished != undefined) {
+            onFinished();
+        }
+    });
+}
 
 class Country {
     constructor() {
@@ -47,12 +50,16 @@ class Country {
     }
     addInts(lineData, member) {
         var ints = lineData.slice(4).map(function (item) { return parseInt(item, 10); });
-        member.length = ints.length;
-        member = member.map(function (v, i) { return v + ints[i]; });
+        for( var i = member.length; i < ints.length; ++i ) {
+            member.push(0);    
+        }
+        for( var i = 0; i < ints.length; ++i ) {
+            member[i] += ints[i];    
+        }
     }
-    addConfirmed(lineData) { addInts(lineData, this.confirmed); }
-    addRecovered(lineData) { addInts(lineData, this.recovered); }
-    addDeaths(lineData) { addInts(lineData, this.deaths); }
+    addConfirmed(lineData) { this.addInts(lineData, this.confirmed); }
+    addRecovered(lineData) { this.addInts(lineData, this.recovered); }
+    addDeaths(lineData) { this.addInts(lineData, this.deaths); }
     average4DayGrowth() {
         return -1;
     }
@@ -64,95 +71,67 @@ class Countries {
     }
     static load(onFinished) {
         var cs = new Countries();
-        new File('confirmed').visitLabels(function(labelsData){ this.labels = labelsData; });
-        new File('confirmed', onFinished).visitLine(function(lineData) {
-            var country = cs.countries.get(line[1]);
-            if(country == undefined) {
-                country = new Country();
-            }
-            country.addConfirmed(lineData);
-        });
-        new File('recovered').visitLine(function(lineData) {
-            var country = cs.countries.get(line[1]);
-            if(country == undefined) {
-                country = new Country();
-            }
-            country.addRecovered(lineData);
-        });
-        new File('deaths').visitLine(function(lineData) {
-            var country = cs.countries.get(line[1]);
-            if(country == undefined) {
-                country = new Country();
-            }
-            country.addDeaths(lineData);
+        visitLabels('confirmed', function(labelsData){ cs.labels = labelsData; }, function() {
+            visitLineData('confirmed', function(lineData) {
+                var country = cs.countries.get(lineData[1]);
+                if(country == undefined) {
+                    country = new Country();
+                    cs.countries.set(lineData[1], country);
+                }
+                country.addConfirmed(lineData);
+            }, function(){
+                visitLineData('recovered', function(lineData) {
+                    var country = cs.countries.get(lineData[1]);
+                    if(country == undefined) {
+                        country = new Country();
+                        cs.countries.set(lineData[1], country);
+                    }
+                    country.addRecovered(lineData);
+                }, function() {
+                    visitLineData('deaths', function(lineData) {
+                        var country = cs.countries.get(lineData[1]);
+                        if(country == undefined) {
+                            country = new Country();
+                            cs.countries.set(lineData[1], country);
+                        }
+                        country.addDeaths(lineData);
+                    }, function(){ onFinished( cs ); });
+                });
+            });
         });
     }
 };
-function addDataSet(chart, label_, color, province, country) {
-    visitFile(label_, function (allText) {
-        var allTextLines = allText.split(/\r\n|\n/);
-        var countryData = [];
-        var datesLabels = allTextLines[0].split(/,/).slice(4);
-        for (index = 1; index < allTextLines.length; index++) {
-            var line = allTextLines[index];
-            var lineData = line.split(/,/);
-            if (lineData[0] == province && lineData[1] == country) {
-                countryData = lineData.slice(4).map(function (item) { return parseInt(item, 10); });
-                break;
-            }
-        }
-        chart.data.labels = datesLabels;
-        chart.data.datasets.push({ label: label_, fill: false, borderColor: color, data: countryData });
-        chart.update();
-    });
-}
-function addChart(chart, province, country) {
-    chart.data.labels = [];
-    chart.data.datasets = [];
-    addDataSet(chart, 'confirmed', 'rgb(255, 99, 132)', province, country);
-    addDataSet(chart, 'recovered', 'rgb(0, 204, 102)', province, country);
-    addDataSet(chart, 'deaths', 'rgb(0, 0, 0)', province, country);
-}
-function fillSelect(country,  countries) {
+
+function fillSelect(chart, country,  countries) {
     var select = document.getElementById('selectCountry');
     countries.countries.forEach(function(value, key){
         var opt = document.createElement('option');
-        // opt.value = index - 1;
-        opt.innerHTML = label;
-        // opt.id = data[0] + '#' + data[1];
+        opt.innerHTML = key;
+        opt.id = key;
         select.appendChild(opt);
-        // if (data[1] == country) {
-        //     select.selectedIndex = index - 1;
-        //     opt.select = true;
-        // }
-
+        if (key == country) {
+             select.selectedIndex = select.length -1;
+             opt.select = true;
+        }
     });
-
-    // visitFile('confirmed', function (allText) {
-    //     var allTextLines = allText.split(/\r\n|\n/);
-    //     for (index = 1; index < allTextLines.length; index++) {
-    //         var line = allTextLines[index];
-    //         var lineParts = line.split(/,/);
-    //         data = lineParts.slice(0, 2);
-    //         label = data[1];
-    //         if (data[0].length > 0) {
-    //             label += " (" + data[0] + ")";
-    //         }
-    //         label += ' confirmed cases ' + lineParts[lineParts.length - 1];
-    //         var select = document.getElementById('selectCountry');
-    //         var opt = document.createElement('option');
-    //         opt.value = index - 1;
-    //         opt.innerHTML = label;
-    //         opt.id = data[0] + '#' + data[1];
-    //         select.appendChild(opt);
-    //         if (data[1] == country) {
-    //             select.selectedIndex = index - 1;
-    //             opt.select = true;
-    //         }
-    //     }
-    // });
+    select.onchange = function () {
+        var option = select.options[select.selectedIndex];
+        fillChart(chart, option.id, countries);
+    };
 }
-function dashboard() {
+function fillChart(chart, country,  countries) {
+    chart.data.labels = countries.labels;
+    chart.data.datasets = [];
+    countries.countries.forEach(function(value, key){
+        if (key == country) {
+            chart.data.datasets.push({ label: 'confirmed', fill: false, borderColor: 'rgb(255, 99, 132)', data: value.confirmed });
+            chart.data.datasets.push({ label: 'recovered', fill: false, borderColor: 'rgb(0, 204, 102)', data: value.recovered });
+            chart.data.datasets.push({ label: 'deaths', fill: false, borderColor: 'rgb(0, 0, 0)', data: value.deaths });
+        }
+    });
+    chart.update();
+}
+ function dashboard() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const key = 'country';
@@ -161,8 +140,11 @@ function dashboard() {
         country = urlParams.get(key);
     }
 
+    var ctx = document.getElementById('theChart').getContext('2d');
+    var chart = new Chart(ctx, { type: 'line', options: {} });
     Countries.load(function(countries) {
-        fillSelect(country,  countries);
+        fillSelect(chart, country,  countries);
+        fillChart(chart, country, countries);
     });
     // var ctx = document.getElementById('theChart').getContext('2d');
     // var chart = new Chart(ctx, { type: 'line', options: {} });
